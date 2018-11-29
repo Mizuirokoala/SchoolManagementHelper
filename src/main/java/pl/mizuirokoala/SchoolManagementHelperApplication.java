519a054987bdb318bfc8853e6107700157ceb681
Damian <Mizuirokoala@gmail.com>
2018-11-30 00:09:01 +0100

    Just started

---
+++ src/main/java/pl/mizuirokoala/SchoolManagementHelperApplication.java
@@ -0,0 +1,13 @@
+package pl.mizuirokoala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementHelperApplication.class,args);
+    }
+
+}
