6beea7195108d95b20482ddb6caf93290ad5d06f
Damian <Mizuirokoala@gmail.com>
2018-11-30 11:47:15 +0100

    fix

--- src/main/java/pl/mizuirokoala/testing/HomeController.java
+++ src/main/java/pl/mizuirokoala/testing/HomeController.java
@@ -1,5 +1,6 @@
 package pl.mizuirokoala.testing;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
