deab349308ae501c317b1f6f6b96d0104e25f020
Damian <Mizuirokoala@gmail.com>
2018-12-03 06:31:06 +0100

    something

--- src/main/java/pl/mizuirokoala/config/SecurityConfig.java
+++ src/main/java/pl/mizuirokoala/config/SecurityConfig.java
@@ -5,35 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.SpringDataJacksonConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    public SpringDataUserDetailsService
-
-
-}
-
-/*public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    public SpringDataUserDetailsService customUserDetailsService() {
-        return new SpringDataUserDetailsService();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/", "/documents/**", "/folders/**").authenticated()
-                .antMatchers("/users/**", "/departments/**").hasRole("ADMIN")
-                .antMatchers("/login").permitAll()
-                .and().formLogin()
-                .loginPage("/login");
-    }
-}
- */
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    public SpringDataUserDetailsService
+//
+//
+//}
